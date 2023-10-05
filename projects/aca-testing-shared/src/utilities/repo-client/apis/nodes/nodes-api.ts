cdd3de8bd8634c375f9725e646bbdf176fdcf031
Mykyta Maliarchuk <maliarchuk99@gmail.com>
2023-10-05 14:09:57 +0200

    [ACS-5994] roll back apiAuth

--- projects/aca-testing-shared/src/utilities/repo-client/apis/nodes/nodes-api.ts
+++ projects/aca-testing-shared/src/utilities/repo-client/apis/nodes/nodes-api.ts
@@ -146,6 +146,7 @@ export class NodesApi extends RepoApi {
 
   async deleteNodeById(id: string, permanent: boolean = true): Promise<void> {
     try {
+      await this.apiAuth();
       await this.nodesApi.deleteNode(id, { permanent });
     } catch (error) {
       this.handleError(`${this.constructor.name} ${this.deleteNodeById.name}`, error);
@@ -174,6 +175,7 @@ export class NodesApi extends RepoApi {
 
   async deleteNodesById(ids: string[], permanent: boolean = true): Promise<void> {
     try {
+      await this.apiAuth();
       await this.nodesApi.deleteNodes(ids, {permanent});
     } catch (error) {
       this.handleError(`${this.constructor.name} ${this.deleteNodesById.name}`, error);
