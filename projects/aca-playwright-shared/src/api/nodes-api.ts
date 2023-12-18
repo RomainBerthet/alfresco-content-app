e4a28064fb8c444bd198d078b7b1067573230d26
SheenaMalhotra182 <sheena.malhotra@contractors.onbase.com>
2023-12-18 16:53:02 +0530

    [ACS-6456] fixed code smells and removed duplication

--- projects/aca-playwright-shared/src/api/nodes-api.ts
+++ projects/aca-playwright-shared/src/api/nodes-api.ts
@@ -405,7 +405,7 @@ export class NodesApi {
   async getNodeProperty(nodeId: string, property: string): Promise<string> {
     try {
       const node = await this.getNodeById(nodeId);
-      return (node.entry.properties && node.entry.properties[property]) || '';
+      return node.entry.properties?.[property] || '';
     } catch (error) {
       console.error(`${this.constructor.name} ${this.getNodeProperty.name}`, error);
       return '';
